0a48aef58c339dd7e69515c579fc5c131c038270
Gaic4o <alstntorl@naver.com>
2024-04-11 21:26:53 +0900

    feat: textFont Component create

--- src/components/textFont/index.tsx
+++ src/components/textFont/index.tsx
@@ -1,4 +1,7 @@
 import { Text3D } from "@react-three/drei";
+import { motion } from "framer-motion-3d";
+import { useSetRecoilState } from "recoil";
+import { isStartScene } from "../../utils/atom";
 
 interface TextProps {
   text: string;
@@ -20,19 +23,28 @@ const CustomText3D: React.FC<TextProps> = ({ text, position }) => (
   </Text3D>
 );
 
-const HowToPlay = () => {
+const TextFont = () => {
+  const setStart = useSetRecoilState(isStartScene);
+
   const controlInstructions: TextProps[] = [
     { text: "플레이 조작 방법", position: [0, 0, 0] },
     { text: "↑", position: [0.5, -0.4, 0] },
     { text: "←↓→", position: [0.3, -0.6, 0] },
   ];
   return (
-    <group position={[0.3, 0, 1]} rotation={[-Math.PI / 2, 0, 0]}>
+    <motion.group
+      onUpdate={() => console.log("진행중")}
+      onAnimationComplete={() => setStart(true)}
+      animate={{ y: [-2, 0], scale: [0, 1] }}
+      transition={{ delay: 1.5, duration: 0.3 }}
+      position={[0.3, 0, 1]}
+      rotation={[-Math.PI / 2, 0, 0]}
+    >
       {controlInstructions.map((instruction) => (
         <CustomText3D key={instruction.text} {...instruction} />
       ))}
-    </group>
+    </motion.group>
   );
 };
 
-export default HowToPlay;
+export default TextFont;
